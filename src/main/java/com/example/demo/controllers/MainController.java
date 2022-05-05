e44ce47ab8e07660137bf77ec6037451b770ff9d
OnF1R <bender987@bk.ru>
2022-05-06 00:27:20 +0300

    Метод добавления данных
    Метод для добавления данных в БД

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -3,9 +3,16 @@ package com.example.demo.controllers;
 import com.example.demo.models.controlUser;
 import com.example.demo.repo.controlUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Controller
 public class MainController {
@@ -18,6 +25,14 @@ public class MainController {
         model.addAttribute("title", "Батутный центр");
         Iterable<controlUser> users = controlUserRepository.findAll();
         model.addAttribute("users", users);
+        return "main";
+    }
+
+    @PostMapping
+    public String addUser(@RequestParam String username, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateEnd, Model model) {
+        LocalDateTime dateStart = LocalDateTime.now();
+        controlUser user = new controlUser(username, dateStart, dateEnd);
+        controlUserRepository.save(user);
         return "main";
     }
 
