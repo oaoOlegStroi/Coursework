5c8689d1ffaece8116ac84e561aac7937a7e093f
OnF1R <bender987@bk.ru>
2022-05-05 20:23:19 +0300

    Создание таблицы БД
    Объявление всех необходимых столбцов таблицы и из получение в переменные

---
+++ src/main/java/com/example/demo/models/controlUser.java
@@ -0,0 +1,53 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+public class controlUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String FIO;
+
+    private Timestamp start_time;
+
+    private Timestamp end_time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public Timestamp getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Timestamp start_time) {
+        this.start_time = start_time;
+    }
+
+    public Timestamp getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(Timestamp end_time) {
+        this.end_time = end_time;
+    }
+
+}
