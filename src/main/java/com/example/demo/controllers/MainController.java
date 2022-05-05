0140057d96f56698532c32a2deb25d21095d61ed
OnF1R <bender987@bk.ru>
2022-05-05 20:22:09 +0300

    Запрос к БД
    Выборка всех данных из controlUserRepository и преобразование их в массив model users

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.controlUser;
+import com.example.demo.repo.controlUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    private controlUserRepository controlUserRepository;
+
     @GetMapping("/")
     public String main(Model model) {
-        model.addAttribute("title", ",Батутный центр");
+        model.addAttribute("title", "Батутный центр");
+        Iterable<controlUser> users = controlUserRepository.findAll();
+        model.addAttribute("users", users);
         return "main";
     }
 
