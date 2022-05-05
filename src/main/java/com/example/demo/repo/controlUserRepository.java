81d9ca9210450f9fa791686f5267fa5758349f36
OnF1R <bender987@bk.ru>
2022-05-05 20:24:02 +0300

    Создание репозитория
    Репозиторий для удобного использования полученных запросом данных из БД

---
+++ src/main/java/com/example/demo/repo/controlUserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repo;
+
+import com.example.demo.models.controlUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface controlUserRepository extends CrudRepository<controlUser, Long> {
+
+}
